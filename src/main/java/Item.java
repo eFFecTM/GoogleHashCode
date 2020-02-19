9061b8914fb2fd101bac32ce585f7417984ba9fc
Jan De Laet <jan.delaet@hotmail.com>
2020-02-19 22:53:06 +0100

    Mockup to load, process and output.

---
+++ src/main/java/Item.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class Item {
+
+    public ArrayList<String> list;
+    public boolean isSomething;
+
+    public Item(ArrayList<String> list, boolean isSomething) {
+        this.list = list;
+        this.isSomething = isSomething;
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,37 @@
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Main {
 
-    public static void main(String... args) {
-        System.out.println("hello world!");
+    public static List<String> tempData1 = new ArrayList<>();
+
+    public static void main(String... args) throws Exception {
+        loadAndProcess("src/main/resources/test.txt");
+        output("output1.txt");
+    }
+
+    private static void loadAndProcess(String path) throws Exception {
+        tempData1 = Files.lines(Paths.get(path)).sorted((s, t) -> {
+            if (true) {
+                return 1;
+            } else if (true) {
+                return -1;
+            } else {
+                return 0;
+            }
+        }).collect(Collectors.toList());
+
+        // process
+        tempData1.forEach(a -> {
+            System.out.println("hello processing1");
+            System.out.println("hello processing2");
+        });
+    }
+
+    private static void output(String fileName) throws Exception {
+        Files.write(Paths.get(fileName), (Iterable<String>)tempData1.stream()::iterator);
     }
 }
