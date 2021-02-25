7676f8703d8fdace108214ac29459bfd82a9b0df
djsimonage <djsimonage@gmail.com>
2021-02-25 20:42:15 +0100

    ADD seconds to street

--- src/main/java/googlehashcode2021/Street.java
+++ src/main/java/googlehashcode2021/Street.java
@@ -2,11 +2,12 @@ package googlehashcode2021;
 
 public class Street {
 
-    public int start, end, time;
+    public int start, end, time, seconds;
 
     public Street(int start, int end, int time) {
         this.start = start;
         this.end = end;
         this.time = time;
+        seconds = 0;
     }
 }
