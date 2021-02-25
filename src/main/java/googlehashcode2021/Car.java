abda0cc547a6dbef11e13cb6b42daff43dd1ac82
Jan De Laet <jan.delaet@hotmail.com>
2021-02-25 19:54:23 +0100

    car cost

--- src/main/java/googlehashcode2021/Car.java
+++ src/main/java/googlehashcode2021/Car.java
@@ -5,8 +5,14 @@ import java.util.Map;
 public class Car {
 
     public Map<String, Street> streets;
+    public int totalTime;
+    public double cost; // lower is better, less streets for the same amount of total time is better
 
     public Car(Map<String, Street> streets) {
         this.streets = streets;
+        for (Street street : streets.values()) {
+            totalTime += street.time;
+        }
+        cost = (double) streets.size() / (double) totalTime;
     }
 }
